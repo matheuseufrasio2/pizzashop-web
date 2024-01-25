bd5b97fdfd810ca82870fb3d2a5a6f18123c8003
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2024-01-25 16:09:12 -0300

    setup toast

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import './global.css'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
+import { Toaster } from './components/ui/sonner'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
+      <Toaster richColors />
       <Helmet titleTemplate="%s | pizza.shop" />
       <RouterProvider router={router} />
     </HelmetProvider>
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -20,7 +21,17 @@ export function Signin() {
   } = useForm<SignInForm>()
 
   async function handleSignin(formData: SignInForm) {
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+      toast.success('Enviamos um link de autenticação pro seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignin(formData),
+        },
+      })
+    } catch {
+      toast.error('Credenciais inválidas.')
+    }
   }
 
   return (
